c51aee1b1cd335644e3bc36cdd8d780d1f00cc3b
CR-2549 <cristian.robles@uabc.edu.mx>
2026-05-17 14:23:24 +0000

    Fix python version for Render

--- blog/urls.py
+++ blog/urls.py
@@ -2,15 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('', views.home, name='home'),
-
     path(
         'catalogo/',
         views.catalogo,
         name='catalogo'
     ),
-
     path(
         'crear/',
         views.crear_game,
