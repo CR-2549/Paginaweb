48b3cdaf25f0f0d9d4235f816530a07baa5bb034
CR-2549 <cristian.robles@uabc.edu.mx>
2026-05-17 15:02:03 +0000

    Add whitenoise

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -112,4 +112,6 @@ STATIC_ROOT = BASE_DIR / "hello_world" / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"git add .
+git commit -m "Add whitenoise"
+git push
